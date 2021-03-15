3b3189502b489b8546cdfecb77e07d6e8048fd47
Dante De Ruwe <dantederuwe@gmail.com>
2021-03-15 15:06:12 +0100

    Introduce lazy loading

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,13 @@ import { PiletApi } from 'piral-tryout';
 import { Link } from 'react-router-dom';
 import FavoriteToggle from './components/FavoriteToggle';
 import './style.scss';
-import Favorites from './components/Favorites';
 import { MovieTileProps } from './models/proptypes';
 
 export function setup(app: PiletApi) {
   app.registerMenu('Favorites', () => <Link to="/favorites">Favorites</Link>);
 
   const MovieTile: React.FC<MovieTileProps> = props => <app.Extension name="MovieTile" params={props}></app.Extension>;
+  const Favorites = React.lazy(() => import('./components/Favorites'));
   app.registerPage('/favorites', () => <Favorites MovieTile={MovieTile}></Favorites>);
 
   app.registerExtension('ListToggle', props => <FavoriteToggle {...props.params}></FavoriteToggle>);
